69592d9908ce05e78601a63069080a881803cba0
padjal <padjal136@gmail.com>
2020-09-21 18:59:46 +0300

    Sem3 HW

---
+++ Module1/Module1_Sem3/HW/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Task01
+{
+    internal class Program
+    {
+
+        public static void FindNumber()
+        {
+            int s = 0;
+            for (int i = 1; i < 1000; i++)
+            {
+                s += i;
+                int res;
+                if (i / 10 > 0)
+                {
+                    res = i % 10;
+                    i = i / 10;
+                }
+
+            }
+        }
+        public static void Main(string[] args)
+        {
+            
+        }
+    }
+}
---
+++ Module1/Module1_Sem3/HW/Task02/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Task02
+{
+    internal class Program
+    {
+        public static int ReOrder(int a)
+        {
+            int r,sum=0;
+            for(int t=a;t!=0;t=t/10)
+            {
+                r=t % 10;
+                sum=sum*10+r;
+            }
+            return sum;
+
+        }
+        public static void Main(string[] args)
+        {
+            if (!int.TryParse(Console.ReadLine(), out int a))
+            {
+                Console.WriteLine("Error");
+                return;
+            }
+            Console.WriteLine(Program.ReOrder(a));
+        }
+    }
+}
