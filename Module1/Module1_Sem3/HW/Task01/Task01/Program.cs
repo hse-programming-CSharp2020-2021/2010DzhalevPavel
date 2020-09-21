a6aca4e42af1b770158df800152daefbdc6f0ac3
padjal <padjal136@gmail.com>
2020-09-21 10:31:05 +0300

    Seminar 3 HW

---
+++ Module1/Module1_Sem3/HW/Task01/Task01/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Task01
+{
+    internal class Program
+    {
+        public static void NaturalNumbers()
+        {
+            
+        }
+        public static void Main(string[] args)
+        {
+        }
+    }
+}
