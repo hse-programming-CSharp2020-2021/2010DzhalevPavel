b8e76c73eb72ae103294fde115aaf2ee5f986cf0
padjal <padjal136@gmail.com>
2020-10-03 12:16:31 +0300

    Seminar 6 CW

---
+++ Module1/Module1_Sem6/CW/Task01/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Task01
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            Console.Write("Array size: ");
+            int n = int.Parse(Console.ReadLine());
+            
+            int[,] a = new int[n,n];
+            int[,] b = new int[n,n];
+
+            //Output first array.
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    if (i < j) a[i,j] = 1;
+                    else if (i == j) a[i,j] = 0;
+                    else a[i,j] = -1;
+                    Console.Write($"{a[i,j],3}");
+                }Console.WriteLine();
+            }Console.WriteLine();
+            
+            //Output second array.
+            //TODO: Finish functionality
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    if (i > j+1) a[i,j] = -1;
+                    else if (i == j) a[i,j] = 0;
+                    else a[i,j] = 1;
+                    Console.Write($"{a[i,j],3}");
+                }Console.WriteLine();
+            }
+
+        }
+    }
+}
---
+++ Module1/Module1_Sem6/CW/Task03/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Task03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //User input.
+            Console.Write("Array size: ");
+            int n = int.Parse(Console.ReadLine());
+            
+            //Initialization of a.
+            int[,] a = new int[n,n];
+            
+            //Output first array.
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    if (i >= j) a[i,j] = '*';
+                    Console.Write($"{(char)a[i,j],3}");
+                }Console.WriteLine();
+            }Console.WriteLine();
+            
+            //Nested arrays method.
+            string[][] b= new String[n][];
+            
+            for (int i = 0; i < n; i++)
+                b[i] = new string[i + 1];
+            
+            //Output second array.
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < b[i].Length; j++)
+                {
+                    b[i][j] = "*";
+                    Console.Write(b[i][j]+" ");
+                }Console.WriteLine();
+            }Console.WriteLine();
+            
+        }
+    }
+}
