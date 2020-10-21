ff6ac3e901339eb094cbd3578bafd3cbc8481bdb
padjal <padjal136@gmail.com>
2020-10-21 16:02:45 +0300

    Task completed

---
+++ Module1/Module1_Sem8/HW/Slide33Task01/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace Slide33Task01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string inputPath = "input.txt";
+            string outputPath = "output.txt";
+            string text = "";
+            try
+            {
+                text = File.ReadAllText(inputPath);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            int[] numbers = new int[text.Split(" ").Length];
+            bool[] newArr = new bool[text.Split(" ").Length];
+            for (int i = 0; i < text.Split(" ").Length; i++)
+            {
+                if (!int.TryParse(text.Split(" ")[i], out numbers[i])
+                    ||numbers[i]>10||numbers[i]<-10)
+                {
+                    Console.WriteLine("Incorrect input");
+                    return;
+                }
+
+                if (numbers[i] >= 0)
+                    newArr[i] = true;
+                else newArr[i] = false;
+            }
+
+            string output = "";
+            
+            foreach (bool value in newArr)
+            {
+                output += $"{value} ";
+            }
+            try
+            {
+                File.WriteAllText(outputPath, output);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            
+        }
+    }
+}
---
+++ Module1/Module1_Sem8/HW/Task06/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Task06
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] TestStrings = {"", "a b c", "Let it be", "All You Need Is    Love"};
+
+            foreach (string str in TestStrings)
+            {
+                Console.WriteLine();
+                string[] tmp = str.Split(' ');
+                // результат выведен без лишних пробелов
+                foreach (string s in tmp)
+                {
+                    Console.Write(s);
+                }
+
+                Console.WriteLine();
+                // добавим индексы, чтобы "увидеть" элементы с пустыми строками
+                for (int i = 0; i < tmp.Length; i++)
+                {
+                    Console.Write(i+1 + ": " + tmp[i]);
+                }
+            }
+        }
+    }
+}
