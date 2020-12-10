94e99bd16f7a361fda588ba7084df435e3e6fddb
padjal <dzhalev.pavel@gmail.com>
2020-12-10 19:14:59 +0200

    classwork dec 10

---
+++ Module2/Dec10_Strings/CW/Numbers/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Text;
+
+namespace Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = Console.ReadLine();
+            
+            Console.WriteLine(ConvertHex2Bin1(s));
+            
+        }
+
+        public static string ConvertHex2Bin1(string HexNumber)
+        {
+            return Convert.ToString(Convert.ToInt64(HexNumber, 16),2);
+        }
+        public static string ConvertHex2Bin2(string HexNumber)
+        {
+            char[] hexSymbols = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'};
+            string[] bin = {"0000", "0001", "0010", "0011", "0100", "0101", "0110", "0111", "1000", "1001", "1010", "1011", "1100", "1101", "1111"}; 
+            StringBuilder sb = new StringBuilder();
+            return sb.ToString();
+            
+        }
+    }
+}
---
+++ Module2/Dec10_Strings/CW/StringMethods/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Text;
+
+namespace StringMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = Console.ReadLine();
+            
+            Console.WriteLine(RemoveSpaces(s));
+            Console.WriteLine($"There are {CountWords(s)} words with more than 4 letters.");
+            Console.WriteLine($"There are {StartingWithVowel(s)} words starting with a vowel.");
+        }
+
+        public static string RemoveSpaces(string s)
+        {
+            
+            StringBuilder sb = new StringBuilder();
+            foreach (var word in s.Split(new char[]{' '}, StringSplitOptions.RemoveEmptyEntries))
+            {
+                sb.Append(word + " ");
+            }
+            return sb.ToString().Substring(0, sb.Length -1);
+        }
+
+        public static int CountWords (string s)
+        {
+            int count = 0;
+            foreach (var word in s.Split(new char[]{' '}, StringSplitOptions.RemoveEmptyEntries))
+            {
+                if (word.Length > 4)
+                    count++;
+            }
+
+            return count;
+        }
+        
+        public static int StartingWithVowel (string s)
+        {
+            string vowels = "еиыэа";
+            int count = 0;
+            foreach (var word in s.Split(new char[]{' '}, StringSplitOptions.RemoveEmptyEntries))
+            {
+                if (vowels.Contains(word[0].ToString().ToLower()))
+                    count++;
+            }
+
+            return count;
+        }
+
+
+
+    }
+}
