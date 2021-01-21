743a57399a299fae1149c5fb1fceac280f2108cf
padjal <dzhalev.pavel@gmail.com>
2021-01-21 17:04:41 +0200

    HW after seminar 1 - Delegates

---
+++ Module3/Module3_Sem1/HW/Robots/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+
+namespace Robots
+{
+    class Robot {
+        // класс для представления робота
+        int x, y; // положение робота на плоскости 
+
+        public void Right() { x++; }      // направо
+        public void Left() { x--; }      // налево
+        public void Forward() { y++; }  // вперед
+        public void Backward() { y--; }  // назад
+
+        public string Position() {  // сообщить координаты
+            return String.Format("The Robot's position: x={0}, y={1}", x, y);
+        }
+    }
+
+    delegate void Steps(); // делегат-тип
+
+    
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter robot commands: ");
+            var commands = Console.ReadLine();
+            
+            Robot rob = new Robot();    // конкретный робот 
+            Steps[] trace = new Steps[commands.Length];
+
+            int count = 0;
+            foreach (char cmd in commands.ToCharArray())
+            {
+                if (cmd == 'R') trace[count] = rob.Right;
+                if (cmd == 'L') trace[count] = rob.Left;
+                if (cmd == 'F') trace[count] = rob.Forward;
+                if (cmd == 'B') trace[count] = rob.Backward;
+                count++;
+            }
+            
+            // сообщить координаты
+            Console.WriteLine("Start:" + rob.Position());    
+ 
+            for (int i = 0; i < trace.Length; i++) {
+                Console.WriteLine("Method={0}, Target={1}",
+                    trace[i].Method, trace[i].Target);
+                trace[i]();
+            }
+
+            Console.WriteLine(rob.Position());     // сообщить координаты
+
+            /*//Robot rob = new Robot();    // конкретный робот 
+            Steps delR = new Steps(rob.Right);      // направо
+            Steps delL = new Steps(rob.Left);       // налево
+            Steps delF = new Steps(rob.Forward);    // вперед
+            Steps delB = new Steps(rob.Backward);   // назад
+            
+            // шаги по диагоналям (многоадресные делегаты):
+            Steps delRF = delR + delF;
+            Steps delRB = delR + delB;
+            Steps delLF = delL + delF;
+            Steps delLB = delL + delB;
+            delLB();
+            Console.WriteLine(rob.Position());     // сообщить координаты
+            delRB();
+            Console.WriteLine(rob.Position());     // сообщить координаты*/
+            
+            
+            Console.WriteLine("To exit this application, click any button.");
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ Module3/Module3_Sem1/HW/TempConvert/TempConverter.cs
@@ -0,0 +1,87 @@
+﻿using System;
+
+namespace TempConvert
+{
+    public delegate double ConvertTemperature(double n);
+
+    //Non-static class creation
+    class TemperatureConverImp
+    {
+        /// <summary>
+        /// Converts degrees Celsius to Fahrenheit
+        /// </summary>
+        /// <param name="n">degrees Celsius</param>
+        /// <returns></returns>
+        public double CelsiusToFahrenheit(double n)
+        {
+            return n * 9 / 5 + 32;
+        }
+
+        /// <summary>
+        /// Converts degrees Fahrenheit to Celsius
+        /// </summary>
+        /// <param name="n">degrees Fahrenheit</param>
+        /// <returns></returns>
+        public double FahrenheitToCelsius(double n)
+        {
+            return (n - 32) * 5 / 9;
+        }
+    }
+
+    static class StaticTempConverters
+    {
+        public static double CelsiusToFahrenheit(double n)
+        {
+            return n * 9 / 5 + 32;
+        }
+        public static double CelsiusToKelvins(double n)
+        {
+            return n - 273.15;
+        }
+        public static double CelsiusToRemoir(double n)
+        {
+            return n/1.25;
+        }
+    }
+
+    static class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            //Initializing of the delegates
+            ConvertTemperature del1 = new TemperatureConverImp().CelsiusToFahrenheit;
+            ConvertTemperature del2 = new TemperatureConverImp().FahrenheitToCelsius;
+
+            //Testing
+            int degrees;
+            do
+            {
+                Console.WriteLine("Enter degrees in Celsius: ");
+            } while (!int.TryParse(Console.ReadLine(), out degrees));
+
+            
+            Console.WriteLine($"This temperature can also be represented by " +
+                              $"{del1(degrees)} degree Fahrenheit.");
+            Console.WriteLine($"Initial temperature is " +
+                              $"{del2(del1(degrees))} degree Celsius.");
+
+            ConvertTemperature[] delArray =
+            {
+                StaticTempConverters.CelsiusToFahrenheit,
+                StaticTempConverters.CelsiusToKelvins,
+                StaticTempConverters.CelsiusToRemoir,
+                new TemperatureConverImp().FahrenheitToCelsius
+            };
+            //and so on and so on ultil all temperatures needed are added.
+
+            for (int i = 0; i < delArray.Length; i++)
+            {
+                Console.WriteLine($"Method: {delArray[i].Method}" +
+                                  $"Target: {delArray[i].Target}" +
+                                  $"Result: {delArray[i](degrees)}");
+            }
+            //Creating an array of delegates    
+        }
+    }
+}
