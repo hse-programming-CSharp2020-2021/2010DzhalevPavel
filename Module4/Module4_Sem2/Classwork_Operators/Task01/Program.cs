a75851e4a882c9f8648db849c3b6fbc653cdf4f7
padjal <padjal136@gmail.com>
2021-04-22 18:43:41 +0300

    Classwork tasks completed - Operators overloading

---
+++ Module4/Module4_Sem2/Classwork_Operators/Task01/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Task01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Bread bread = new Bread { Weight = 80 };
+            Butter butter = new Butter { Weight = 20 };
+            Sandwich sandwich = bread + butter;
+            Console.WriteLine(sandwich.Weight);  // 100
+        }
+    }
+    // хлеб
+    class Bread
+    {
+        public int Weight { get; set; } // масса
+
+        public static Sandwich operator +(Bread bread, Butter butter)
+        {
+            var sand = new Sandwich();
+            sand.Weight = bread.Weight + butter.Weight;
+            return sand;
+        }
+    }
+
+
+    // масло
+    class Butter
+    {
+        public int Weight { get; set; } // масса
+    }
+
+
+    // бутерброт
+    class Sandwich
+    {
+        public int Weight { get; set; } // масса
+    }
+}
---
+++ Module4/Module4_Sem2/Classwork_Operators/Task02/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Task02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            State state1 = new State();
+            State state2 =new State();
+            State state3 = state1 + state2;
+            bool isGreater = state1 > state2;
+        }
+    }
+
+    class State
+    {
+        public decimal Population{get;set;}
+        public decimal Area { get; set; }
+
+        public static State operator +(State state1, State state2)
+        {
+            var newState = new State();
+            newState.Area = state1.Area + state2.Area;
+            newState.Population = state1.Population + state2.Population;
+            return newState;
+        }
+
+        //State objects are compared by their area.
+        public static bool operator >(State state1, State state2)
+        {
+            if (state1.Area > state2.Area)
+                return true;
+            return false;
+        }
+        public static bool operator <(State state1, State state2)
+        {
+            if (state1.Area < state2.Area)
+                return true;
+            return false;
+        }
+    }
+}
