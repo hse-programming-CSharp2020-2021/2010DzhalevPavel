6594b9cc7fb1002560db8b8003dd3a9c54f2ecac
padjal <padjal136@gmail.com>
2021-06-10 20:29:36 +0300

    One more regex task

---
+++ Module4/Module4_Sem8/Classwork - Regex/Task03/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Net.Http;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace Task03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var text = GetCode("https://ru.wikipedia.org/wiki/Заглавная_страница").Result;
+            FindMatches(text);
+        }
+        
+        static async Task<string> GetCode(string link) {
+            using HttpClient client = new HttpClient();
+            var wikiLink = link;
+            var response = await client.GetAsync(wikiLink);
+            string htmlText = await response.Content.ReadAsStringAsync();
+            Console.WriteLine($"Скачали ответ: {htmlText.Length} символов");
+            return htmlText;
+        }
+
+        static void FindMatches(string htmlText)
+        {
+            // Находим все подстроки, подходящие по шаблону:
+            MatchCollection linksCollection = Regex.Matches(htmlText,
+                @"<img.*src=""(?<name>.*\.jpg)"".*>");
+ 
+            foreach (Match link in linksCollection)
+                Console.WriteLine($"{link.Groups["name"].Value}");
+            
+            
+        }
+    }
+}
