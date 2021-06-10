ec46052327830da2ab13bcb60c5e539b77829c8e
padjal <padjal136@gmail.com>
2021-06-10 20:07:21 +0300

    Regex Excercises

---
+++ Module4/Module4_Sem8/Classwork - Regex/Task01/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace Task01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string match = "^(<link).*\\.js.*>";
+            var input = Console.ReadLine();
+
+            string match2 = "#Goods.*(Code[0-9]*)*";
+
+            var rand = new Random();
+            var result = Regex.Replace(input, match2, rand.Next(1,10000).ToString());
+
+            Console.WriteLine(result);
+        }
+    }
+}
